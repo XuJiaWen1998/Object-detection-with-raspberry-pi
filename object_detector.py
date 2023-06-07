b5219063a536b46d1a6773f1d23a0a7d6bc3b004
Jiawen Xu <jix034@ucsd.edu>
2023-06-06 22:08:00 -0700

    update

--- object_detector.py
+++ object_detector.py
@@ -20,16 +20,23 @@ class object_detector():
         self.net.setInputMean((127.5, 127.5, 127.5))
         self.net.setInputSwapRB(True)
 
-    def getObjects(self,img):
+    def getObjects(self, img, draw=True, objects=[]):
         classIds, confs, bbox = self.net.detect(img, confThreshold=self.thres, nmsThreshold=self.nms_threshold)
+        if len(objects) == 0:
+            objects = self.classNames
+        objectInfo = []
         if len(classIds) != 0:
             for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                cv2.putText(img,self.classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-                cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-        return img
+                className = self.classNames[classId-1]
+                if className in objects:
+                    objectInfo.append([box, className])
+                    if (draw):
+                        cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                        cv2.putText(img,className.upper(),(box[0]+10,box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                        cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+        return img, objectInfo
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
@@ -38,6 +45,8 @@ if __name__ == "__main__":
     od = object_detector()
     while True:
         success, img = cap.read()
-        result = od.getObjects(img)
+        target_object = []
+        result, objectInfo = od.getObjects(img, draw=True, objects=target_object)
+        print(objectInfo)
         cv2.imshow("Output", result)
         cv2.waitKey(1)
